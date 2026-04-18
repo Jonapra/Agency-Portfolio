e21e2adae85f7e99447b1e805dbbc88a2a89ec1e
Ankit Pradhan <ankitpradhan996@gmail.com>
2026-04-18 22:23:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+// This is a main js
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
